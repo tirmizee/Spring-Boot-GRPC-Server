511337fbf9bf8301282d937fecde45cf87d1a847
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-03-14 21:53:55 +0700

    update proto

--- src/main/java/com/tirmizee/service/HelloServiceImpl.java
+++ src/main/java/com/tirmizee/service/HelloServiceImpl.java
@@ -3,8 +3,8 @@ package com.tirmizee.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.tirmizee.grpc.Greeting;
 import com.tirmizee.grpc.HelloWorldServiceGrpc;
+import com.tirmizee.grpc.Message;
 import com.tirmizee.grpc.Person;
 
 import io.grpc.stub.StreamObserver;
@@ -14,18 +14,10 @@ import net.devh.boot.grpc.server.service.GrpcService;
 public class HelloServiceImpl extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HelloServiceImpl.class);
-	
-	@Override
-	public void sayHello(Person request, StreamObserver<Greeting> responseObserver) {
-		LOGGER.info("server received {}", request);
-		String message = "Hello " + request.getFirstName() + " "
-			        + request.getLastName() + "!";
-			    Greeting greeting =
-			        Greeting.newBuilder().setMessage(message).build();
-			    LOGGER.info("server responded {}", greeting);
 
-	    responseObserver.onNext(greeting);
-	    responseObserver.onCompleted();
+	@Override
+	public void sayHello(Person request, StreamObserver<Message> responseObserver) {
+		super.sayHello(request, responseObserver);
 	}
 
 }
